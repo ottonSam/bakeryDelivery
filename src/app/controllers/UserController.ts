40f054d360e8cec3673aef207256d90f6efcaf7b
o maia <omaia@pop-os.localdomain>
2021-03-11 21:38:36 -0300

    finish auth api

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -4,6 +4,10 @@ import { getRepository } from 'typeorm';
 import User from '../models/User';
 
 class UserController{
+  index(req: Request, res: Response) {
+    return res.json({
+      'userId': req.userId});
+  }
   async store(req: Request, res: Response) {
     const repository = getRepository(User);
     const { email, cpf, telefone, data_de_nascimento, logadouro, numero, estado,
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import AuthController from './app/controllers/AuthController';
+import authMiddleware from './app/middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/users', UserController.store);
+router.get('/users', authMiddleware, UserController.index);
+router.post('/auth', AuthController.authenticate); 
 
 export default router;
