14393007e33f04a6d3e9ea44deb59903477ab17a
ottonSam <ottonsam@gmail.com>
2021-03-14 18:03:14 -0300

    heroku config port

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('🔥 Server started'));
+app.listen(process.env.PORT || 3333, () => console.log('🔥 Server started'));
