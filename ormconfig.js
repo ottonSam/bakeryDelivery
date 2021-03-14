a59c15470857fb486701a4cf82ac5d079b0bcf22
ottonSam <ottonsam@gmail.com>
2021-03-14 16:50:18 -0300

    review for heroku deploy

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "type": "postgres",
+  "url": process.env.DATABASE_URL,
+  "entities": [
+    process.env.ENTITIES
+  ],
+  "migrations": [
+    process.env.MIGRATIONS
+  ],
+  "cli": {
+    "migrationsDir": "src/database/migrations"
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
+import dotenv from 'dotenv';
 import 'reflect-metadata';
 import express from 'express';
 
 import './database/connect';
 import routes from './routes';
 
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('🔥 Server started on localHost'));
+app.listen(3333, () => console.log('🔥 Server started'));
