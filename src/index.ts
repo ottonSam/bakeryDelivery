7029e3c27b857d31bec509db8120d5378bf1bb0e
ottonSam <ottonsam@gmail.com>
2021-03-14 23:37:50 -0300

    cors config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import 'reflect-metadata';
+import cors from 'cors';
 import express from 'express';
 
 import './database/connect';
@@ -7,7 +8,13 @@ import routes from './routes';
 
 dotenv.config();
 
+const corsOptions = {
+  origin: '*',
+}
+
 const app = express();
+
+app.use(cors(corsOptions))
 
 app.use(express.json());
 app.use(routes);
